5b5445f70c4e5e5adfb8d6700a21fb6c175729bd
Thanos Diamantidis <thanosdi@live.com>
2018-05-22 13:17:32 +0300

    update title and imdb resolver

--- src/index.js
+++ src/index.js
@@ -10,68 +10,66 @@ const {
 
 const manifest = {
 	'id': 'org.stremio.piratebay',
-	'version': '1.2.0',
+	'version': '1.3.0',
 	'name': 'PirateBay Addon',
 	'description': 'Fetch PirateBay entries on a single episode or series.',
 	'icon': 'https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png',
 	'logo': 'https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png',
 	'isFree': true,
 	'email': 'thanosdi@live.com',
-	'endpoint': 'https://td-stremio-plugin.herokuapp.com/stremio/v1',
-	'types': ['movie', 'series', 'tv', 'channel'],
-	'idProperty': ['ptb_id', 'imdb_id'], // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
+	'endpoint': 'https://piratebay-stremio-addon.herokuapp.com/stremio/v1',
+	'types': ['movie', 'series'],
+	'idProperty': 'imdb_id', // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
 	// We need this for pre-4.0 Stremio, it's the obsolete equivalent of types/idProperty
-	'filter': { 'query.imdb_id': { '$exists': true }, 'query.type': { '$in':['movie', 'series', 'tv', 'channel'] } }
+	'filter': { 'query.imdb_id': { '$exists': true }, 'query.type': { '$in':['series','movie'] } }
 };
 
 const manifestLocal = {
-	'id': 'org.stremio.piratebay-local',
-	'version': '1.2.0',
-	'name': 'PirateBay Addon-local',
-	'description': 'Fetch PirateBay entries on a single episode or series.-local',
+	'id': 'org.stremio.piratebay',
+	'version': '1.3.0',
+	'name': 'PirateBay Addon',
+	'description': 'Fetch PirateBay entries on a single episode or series.',
 	'icon': 'https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png',
 	'logo': 'https://files.gamebanana.com/img/ico/sprays/apirateslifeforme2007tpbpicrip.png',
 	'isFree': true,
 	'email': 'thanosdi@live.com',
 	'endpoint': 'http://localhost:7000/stremioget/stremio/v1',
-	'types': ['movie', 'series', 'tv', 'channel'],
-	'idProperty': ['ptb_id', 'imdb_id'], // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
+	'types': ['movie', 'series'],
+	'idProperty': 'imdb_id', // the property to use as an ID for your add-on; your add-on will be preferred for items with that property; can be an array
 	// We need this for pre-4.0 Stremio, it's the obsolete equivalent of types/idProperty
-	'filter': { 'query.imdb_id': { '$exists': true }, 'query.type': { '$in':['movie', 'series', 'tv', 'channel'] } }
+	'filter': { 'query.imdb_id': { '$exists': true }, 'query.type': { '$in':['series','movie'] } }
 };
 
 const addon = new Stremio.Server({
 	'meta.search': async (args, callback) => {
 		console.log('meta.search args', args);
 		const query = args.query;
-		let results = [];
 		try {
-			results = await ptbSearch(query);
-			console.log('meta.search results', results);
+			const {results} = await ptbSearch(query);
+			const response = results.slice(0, 4).map( episode => {
+				const id = `${episode.magnetLink}|||${episode.name}|||S:${episode.seeders}`;
+				const encodedData = new Buffer(id).toString('base64');
+				console.log('encodedData', encodedData);
+				return {
+					id:`ptb_id:${encodedData}`,
+					ptb_id: `${encodedData}`,
+					video_id: `${episode.name.split('.').join(' ')} , S:${episode.seeders}`,
+					name: `${episode.name.split('.').join(' ')} , S:${episode.seeders}`,
+					poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/The_Pirate_Bay_logo.svg/2000px-The_Pirate_Bay_logo.svg.png',
+					posterShape: 'regular',
+					banner: 'http://thetvdb.com/banners/graphical/78804-g44.jpg',
+					isFree: true,
+					type: 'movie'
+				};
+			});
+			return callback(null, {
+				query,
+				results: response
+			});
 		} catch (e) {
 			console.log('e.message', e.message);
 		}
-		const response = results.slice(0, 4).map( episode => {
-			const id = `${episode.magnetLink}|||${episode.name}|||S:${episode.seeders}`;
-			const encodedData = new Buffer(id).toString('base64');
-			console.log('encodedData', encodedData);
-			return {
-				id:`ptb_id:${encodedData}`,
-				ptb_id: `${encodedData}`,
-				video_id: `${episode.name.split('.').join(' ')} , S:${episode.seeders}`,
-				name: `${episode.name.split('.').join(' ')} , S:${episode.seeders}`,
-				poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/The_Pirate_Bay_logo.svg/2000px-The_Pirate_Bay_logo.svg.png',
-				posterShape: 'regular',
-				banner: 'http://thetvdb.com/banners/graphical/78804-g44.jpg',
-				isFree: true,
-				type: 'movie'
-			};
-		});
-		console.log('meta.search', response);
-		return callback(null, {
-			query,
-			results: response
-		});
+
 
 	},
 	'meta.get': async function(args, callback, user) {
@@ -100,6 +98,7 @@ const addon = new Stremio.Server({
 
 	},
 	'stream.find': async (args, callback) => {
+		console.log('args', args);
 		/* Handle search results with ptb_id */
 		if (args.query.type === 'movie' && args.query.ptb_id) {
 			const decodedData = new Buffer(args.query.ptb_id, 'base64').toString('ascii');
@@ -123,28 +122,29 @@ const addon = new Stremio.Server({
 					file.title = file.title.split('.').join(' ');
 					return file;
 				});
-			console.log(`stream.find: ${args.query.ptb_id}`, results);
+			// console.log(`stream.find: ${args.query.ptb_id}`, results);
 			return callback(null, results);
 		}
 		/* Handle non ptb_id results*/
 		const title = await createTitle(args);
-
-		const resolve = await ptbSearch(title, args.query.type);
-
-		const results = resolve.slice(0, 4).map( episode => {
-			const {infoHash, announce } = magnet.decode(episode.magnetLink);
-			const availability = episode.seeders == 0 ? 0 : episode.seeders < 5 ? 1 : 2;
-			const detail = `${episode.name} S:${episode.seeders}`;
-			return {
-				infoHash,
-				name: 'PTB',
-				title: detail,
-				isFree: true,
-				availability
-			};
-		});
-		console.log('stream.find', results);
-		return callback(null, results);
+		try {
+			const {results} = await ptbSearch(title, args.query.type);
+			const resolve = results.slice(0, 4).map( episode => {
+				const {infoHash, announce } = magnet.decode(episode.magnetLink);
+				const availability = episode.seeders == 0 ? 0 : episode.seeders < 5 ? 1 : 2;
+				const detail = `${episode.name}
+👤 ${episode.seeders}`;
+				return {
+					infoHash,
+					name: 'PTB',
+					title: detail,
+					availability
+				};
+			});
+			return callback(null, resolve);
+		} catch (e) {
+			console.log('ptbsearch error:', e.message);
+		}
 	},
 
 }, manifest);
